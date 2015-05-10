0f4bf08b56a0883508803cb096ba29ac5ebe88c1
Robert Tuck <robtuck@gmail.com>
2015-05-10 16:03:49 -0400

    initial commit

---
+++ plot2.R
@@ -0,0 +1,5 @@
+powerData <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+powerData$DateTime <- paste(powerData$Date, powerData$Time)
+powerData$DateTime <- strptime(powerData$DateTime, "%d/%m/%Y %H:%M:%S")
+dataSet <- subset(powerData, DateTime >= as.POSIXct("2007-02-01 00:00:00") & DateTime < as.POSIXct("2007-02-03 00:00:00"))
+plot(dataSet$DateTime, dataSet$Global_active_power, type = "l")
---
+++ plot3.R
@@ -0,0 +1,7 @@
+powerData <- read.table("./household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+powerData$DateTime <- paste(powerData$Date, powerData$Time)
+powerData$DateTime <- strptime(powerData$DateTime, "%d/%m/%Y %H:%M:%S")
+dataSet <- subset(powerData, DateTime >= as.POSIXct("2007-02-01 00:00:00") & DateTime < as.POSIXct("2007-02-03 00:00:00"))
+plot(dataSet$DateTime, dataSet$Sub_metering_1, type = "l", col = "black")
+lines(dataSet$DateTime, dataSet$Sub_metering_2, type = "l", col = "red")
+lines(dataSet$DateTime, dataSet$Sub_metering_3, type = "l", col = "blue")
